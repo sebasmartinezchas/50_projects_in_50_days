2fe16bb737574822f119ce46c1b2932586b2a9f7
sebasmartinezchas <sebastianmartinezchas@gmail.com>
2023-10-31 12:08:29 -0300

    drink water finished

--- drink_water/script.js
+++ drink_water/script.js
@@ -2,6 +2,7 @@ const smallCups = document.querySelectorAll(".cup-small");
 const liters = document.getElementById("liters");
 const percentage = document.getElementById("percentage");
 const remained = document.getElementById("remained");
+updateBigCup();
 
 smallCups.forEach((cup, idx) => {
   cup.addEventListener("click", () => highlightCups(idx));
@@ -21,4 +22,25 @@ function highlightCups(idx) {
       cup.classList.remove("full");
     }
   });
+  updateBigCup();
+}
+
+function updateBigCup(idx) {
+  const fullCups = document.querySelectorAll(".cup-small.full").length;
+  const totalCups = smallCups.length;
+  if (fullCups === 0) {
+    percentage.style.visibility = "hidden";
+    percentage.style.height = 0;
+  } else {
+    percentage.style.visibility = "visible";
+    percentage.style.height = `${(fullCups / totalCups) * 330}px`;
+    percentage.innerText = `${(fullCups / totalCups) * 100}%`;
+  }
+  if (fullCups === totalCups) {
+    remained.style.visibility = "hidden";
+    remained.style.height = 0;
+  } else {
+    remained.style.visibility = "visible";
+    liters.innerText = `${2 - (250 * fullCups) / 1000}L`;
+  }
 }
