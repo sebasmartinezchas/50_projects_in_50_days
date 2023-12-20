d11e27ef35753d239613ff849485fa3c0ff21120
sebasmartinezchas <sebastianmartinezchas@gmail.com>
2023-12-20 13:26:17 -0300

    random_image_feed

---
+++ random_image_feed/script.js
@@ -0,0 +1,18 @@
+const container=document.querySelector('.container')
+const unplashURL='https://source.unsplash.com/random/'
+const rows=10
+
+
+for(let i=0;i<rows *3;i++){
+    const img=document.createElement('img')
+    img.src=`${unplashURL}${getRandomSize()}`
+    container.appendChild(img)
+}
+
+function getRandomNr(){
+    return Math.floor(Math.random() * 10)+300;
+}
+
+function getRandomSize(){
+ return `${getRandomNr()}x${getRandomNr()}`
+}
